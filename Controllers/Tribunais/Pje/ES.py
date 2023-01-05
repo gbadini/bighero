5989f04132627cfb22e9d165a7791469c69aeefd
gbadini <gbadini@gmail.com>
2023-01-05 09:00:22 -0300

    Mudança URL Pje ES

--- Controllers/Tribunais/Pje/ES.py
+++ Controllers/Tribunais/Pje/ES.py
@@ -5,5 +5,5 @@ class ES(Pje):
 
     def __init__(self):
         super().__init__()
-        self.pagina_inicial = "https://sistemas.tjes.jus.br/pje/login.seam"
-        self.pagina_busca = "https://sistemas.tjes.jus.br/pje/Processo/CadastroPeticaoAvulsa/peticaoavulsa.seam"
+        self.pagina_inicial = "https://pje.tjes.jus.br/pje/login.seam"
+        self.pagina_busca = "https://pje.tjes.jus.br/pje/Processo/CadastroPeticaoAvulsa/peticaoavulsa.seam"
--- Controllers/Tribunais/Pje/ES2g.py
+++ Controllers/Tribunais/Pje/ES2g.py
@@ -5,5 +5,5 @@ class ES2g(Pje2g):
 
     def __init__(self):
         super().__init__()
-        self.pagina_inicial = "https://sistemas.tjes.jus.br/pje2g/login.seam"
-        self.pagina_busca = "https://sistemas.tjes.jus.br/pje2g/Processo/CadastroPeticaoAvulsa/peticaoavulsa.seam"
+        self.pagina_inicial = "https://pje.tjes.jus.br/pje2g/login.seam"
+        self.pagina_busca = "https://pje.tjes.jus.br/pje2g/Processo/CadastroPeticaoAvulsa/peticaoavulsa.seam"
